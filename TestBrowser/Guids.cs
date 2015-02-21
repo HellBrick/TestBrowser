56246cc2cfbf3fea549d63225092d2bfb924c3af
HellBrick <hellbrick@gmail.com>
2015-02-21 13:14:43 +0300

    Initial commit

---
+++ TestBrowser/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace HellBrick.TestBrowser
+{
+	static class GuidList
+	{
+		public const string guidTestBrowserPkgString = "95628654-1ff6-4434-bb0e-d90a65c763f0";
+		public const string guidTestBrowserCmdSetString = "bc1b1326-1b3d-49fa-a255-b22ba976873f";
+		public const string guidToolWindowPersistanceString = "a771a04c-f8c6-4f54-8bf1-2e64e7cdd770";
+
+		public static readonly Guid guidTestBrowserCmdSet = new Guid( guidTestBrowserCmdSetString );
+	};
+}
---
+++ TestBrowser/PkgCmdID.cs
@@ -0,0 +1,11 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace HellBrick.TestBrowser
+{
+	static class PkgCmdIDList
+	{
+		public const uint cmdidTestBrowserTool = 0x101;
+	};
+}
