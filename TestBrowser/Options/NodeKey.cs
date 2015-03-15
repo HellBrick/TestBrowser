caa2f1c692db1dc9a092b8edb3629753f790c151
HellBrick <hellbrick@gmail.com>
2015-03-15 22:54:06 +0300

    Proof of concept of saving expanded nodes

---
+++ TestBrowser/Options/NodeKey.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HellBrick.TestBrowser.Models;
+
+namespace HellBrick.TestBrowser.Options
+{
+	public class NodeKey: IEquatable<NodeKey>
+	{
+		public NodeKey( NodeType type, string name )
+		{
+			Type = type;
+			Name = name;
+		}
+
+		public NodeType Type { get; private set; }
+		public string Name { get; private set; }
+
+		public override string ToString()
+		{
+			return String.Format( "[{0}] {1}", Type, Name );
+		}
+
+		#region IEquatable<NodeKey> Members
+
+		public bool Equals( NodeKey other )
+		{
+			return
+				this.Name == other.Name &&
+				this.Type == other.Type;
+		}
+
+		public override bool Equals( object obj )
+		{
+			return
+				obj is NodeKey &&
+				this.Equals( (NodeKey) obj );
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				int hash = 17;
+				const int prime = 23;
+
+				hash = hash * prime + Type.GetHashCode();
+				hash = hash * prime + Name.GetHashCode();
+				return hash;
+			}
+		}
+
+		#endregion
+	}
+}
