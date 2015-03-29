7235bad79262b9bc906788e393e244a1e77c5f6a
HellBrick <hellbrick@gmail.com>
2015-03-29 17:26:59 +0300

    Details panel is moved to the right when the width of the tool window allows it (fixes #4)

---
+++ TestBrowser/Common/IsValueBiggerThanConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HellBrick.TestBrowser.Common
+{
+	public class IsValueBiggerThanConverter: IValueConverter
+	{
+		public double Threshold { get; set; }
+
+		#region IValueConverter Members
+
+		public object Convert( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+		{
+			return (double) value > Threshold;
+		}
+
+		public object ConvertBack( object value, Type targetType, object parameter, System.Globalization.CultureInfo culture )
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
