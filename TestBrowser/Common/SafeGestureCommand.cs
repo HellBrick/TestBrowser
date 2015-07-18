619cb05067c6851b6a28bd40cb03b47709817a8b
HellBrick <hellbrick@gmail.com>
2015-07-18 15:22:53 +0300

    Added SafeGestureCommand

---
+++ TestBrowser/Common/SafeGestureCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.VisualStudio.TestWindow.Controller;
+
+namespace HellBrick.TestBrowser.Common
+{
+	public class SafeGestureCommand : SafeCommand
+	{
+		public SafeGestureCommand( SafeDispatcher dispatcher, Action execute, string text, KeyGesture gesture )
+			: base( dispatcher, execute, text )
+		{
+			Gesture = gesture;
+		}
+
+		public KeyGesture Gesture { get; }
+		public string GestureText => Gesture.GetDisplayStringForCulture( CultureInfo.InvariantCulture );
+	}
+}
